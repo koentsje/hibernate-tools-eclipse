11010f5c24788d28de5ec27b5a7fcfb40f52f9d9
Koen Aers <koen.aers@gmail.com>
2026-04-01 12:58:53 +0200

    Remove JBoss Tools dependencies to make the project build standalone
    The JBoss Tools infrastructure (parent POM, target platform, P2 repos)
    is no longer reliably available, causing build failures. This commit
    makes the Hibernate Tools Eclipse plugin self-contained by:
    
    - Replacing the org.jboss.tools:parent POM with inline Tycho build config
    - Replacing JBoss Tools P2 repos with standard Eclipse 2024-03 repos
    - Removing the JBoss Tools target platform artifact
    - Removing hibernate.xml/xml.ui plugins (depend on org.jboss.tools.common.model)
    - Removing org.jboss.tools.usage dependency (UsageTracker is now a no-op)
    - Removing org.jboss.tools.releng:enforcer-rules from runtime modules
    - Removing license-feature references to org.jboss.tools.foundation
    - Excluding test modules that depend on org.jboss.tools.tests/reddeer
    - Adding .idea/ to .gitignore
    
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugin/runtime/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/UsageTracker.java
+++ orm/plugin/runtime/org.jboss.tools.hibernate.runtime.common/src/org/jboss/tools/hibernate/runtime/common/UsageTracker.java
@@ -1,38 +1,21 @@
 package org.jboss.tools.hibernate.runtime.common;
 
-import org.jboss.tools.hibernate.runtime.common.internal.HibernateRuntimeCommon;
-import org.jboss.tools.usage.event.UsageEventType;
-import org.jboss.tools.usage.event.UsageReporter;
-
 public class UsageTracker {
-	
+
 	private static UsageTracker INSTANCE;
-	
+
 	public static UsageTracker getInstance() {
 		if (INSTANCE == null) {
 			INSTANCE = new UsageTracker();
 		}
 		return INSTANCE;
 	}
-	
-	private UsageEventType newConfigurationEventType;
-	
+
 	private UsageTracker() {
-		initializeUsageEventType();
 	}
-	
-	private void initializeUsageEventType() {
-		newConfigurationEventType = new UsageEventType(
-				"hibernate", 
-				UsageEventType.getVersion(HibernateRuntimeCommon.getDefault()), 
-				null, 
-				"new configuration",
-				"Hibernate runtime version");
-		UsageReporter.getInstance().registerEvent(newConfigurationEventType);
-	}
-	
+
 	public void trackNewConfigurationEvent(String hibernateVersion) {
-		UsageReporter.getInstance().trackEvent(newConfigurationEventType.event(hibernateVersion));
+		// Usage tracking removed - was dependent on org.jboss.tools.usage
 	}
 
 }
