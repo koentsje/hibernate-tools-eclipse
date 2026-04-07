1c3abeddee4a4174e6e7354971501eb0262124aa
Koen Aers <koen.aers@gmail.com>
2026-04-07 16:12:22 +0200

    #5 Merge org.hibernate.tool.eclipse.orm.console.ui into org.hibernate.tool.eclipse.orm.ui   - Move all 222 source files, icons, doc/, schema/ into orm.ui   - Use HibernateConsolePlugin as bundle activator, convert UiPlugin to     utility class delegating to HibernateConsolePlugin   - Merge MANIFEST.MF: combine Require-Bundle and Export-Package entries   - Merge plugin.xml: perspectives, views, wizards, editors, launch configs,     exporters, preference pages, action sets, key bindings   - Merge plugin.properties   - Update statusHandler plugin reference to org.hibernate.tool.eclipse.orm.ui   - Remove console.ui from Require-Bundle in 7 dependent plugins   - Remove console.ui from orm and search feature.xml files   - Update emma.instrument.bundles in test pom.xml   - Delete org.hibernate.tool.eclipse.orm.console.ui plugin
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

--- orm/plugins/ui/main/org.hibernate.tool.eclipse.orm.ui/src/org/hibernate/tool/eclipse/diagram/UiPlugin.java
+++ orm/plugins/ui/main/org.hibernate.tool.eclipse.orm.ui/src/org/hibernate/tool/eclipse/diagram/UiPlugin.java
@@ -18,48 +18,31 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.plugin.*;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.jface.resource.ImageDescriptor;
-import org.osgi.framework.BundleContext;
+import org.hibernate.eclipse.console.HibernateConsolePlugin;
 
 /**
  *
  */
-public class UiPlugin extends AbstractUIPlugin {
+public class UiPlugin {
 
 	public final static String ID = "org.hibernate.tool.eclipse.ui"; //$NON-NLS-1$
-	
-	//The shared instance.
-	private static UiPlugin plugin;
-	
-	public UiPlugin() {
-		setPlugin(this);
-	}
 
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
-
-	public void stop(BundleContext context) throws Exception {
-		super.stop(context);
-		setPlugin(null);
-	}
+	private static final UiPlugin INSTANCE = new UiPlugin();
 
 	public static UiPlugin getDefault() {
-		return plugin;
-	}
-
-	private static void setPlugin(UiPlugin plugin) {
-		UiPlugin.plugin = plugin;
+		return INSTANCE;
 	}
 
 	public static ImageDescriptor getImageDescriptor(String path) {
-		return AbstractUIPlugin.imageDescriptorFromPlugin(ID, path);
+		return AbstractUIPlugin.imageDescriptorFromPlugin(
+				HibernateConsolePlugin.getDefault().getBundle().getSymbolicName(), path);
 	}
 
 	public static ImageDescriptor getImageDescriptor2(String name) {
 		final String iconPath = "images/"; //$NON-NLS-1$
-		final URL installURL = getDefault().getBundle().getEntry("/"); //$NON-NLS-1$
+		final URL installURL = HibernateConsolePlugin.getDefault().getBundle().getEntry("/"); //$NON-NLS-1$
 		URL url = null;
 		try {
 			url = new URL(installURL, iconPath + name);
@@ -78,5 +61,9 @@ public class UiPlugin extends AbstractUIPlugin {
 	    IWorkbench workbench = PlatformUI.getWorkbench();
 	    IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
 	    return window.getShell();
+	}
+
+	public org.eclipse.core.runtime.IPath getStateLocation() {
+		return HibernateConsolePlugin.getDefault().getStateLocation();
 	}
 }
