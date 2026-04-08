ec50c11e8d6da79f6f2af2df548fb5a9eda5db07
Koen Aers <koen.aers@gmail.com>
2026-04-08 19:27:50 +0200

    #5 Split org.hibernate.tool.eclipse.orm.test into core and UI test plugins   - Create org.hibernate.tool.eclipse.orm.core.test at     orm/plugins/core/test/ with headless tests (BeanShellIntegration,     DriverDelete, JavaFormatting, KnownConfigurations, QueryParameters,     Refactoring, Sanity)   - Create org.hibernate.tool.eclipse.orm.ui.test at     orm/plugins/ui/test/ with workbench-dependent tests (mapping tests,     editor tests, perspective, diagram, code generation, exporters)   - Core test has no UI dependencies; UI test retains full dependency set   - Rename packages to org.hibernate.tool.eclipse.orm.core.test and     org.hibernate.tool.eclipse.orm.ui.test respectively   - Remove original org.hibernate.tool.eclipse.orm.test plugin   - Update feature.xml and parent pom module references
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/plugins/ui/test/org.hibernate.tool.eclipse.orm.ui.test/src/org/hibernate/tool/eclipse/orm/ui/test/utils/TestConsoleConfigurationPreferences.java
@@ -0,0 +1,101 @@
+package org.hibernate.tool.eclipse.orm.ui.test.utils;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Properties;
+
+import org.hibernate.console.preferences.ConsoleConfigurationPreferences;
+import org.hibernate.tool.eclipse.runtime.spi.IEnvironment;
+import org.hibernate.tool.eclipse.runtime.spi.IService;
+import org.hibernate.tool.eclipse.runtime.spi.RuntimeServiceManager;
+import org.w3c.dom.Element;
+
+public class TestConsoleConfigurationPreferences implements ConsoleConfigurationPreferences {
+
+	private File cfgXmlFile = null;
+	
+	public TestConsoleConfigurationPreferences(File file) {
+		cfgXmlFile = file;
+	}
+	
+	public void setName(String name) {
+		throw new RuntimeException("not supported");
+	}
+
+	public void readStateFrom(Element element) {
+		throw new RuntimeException("not supported");
+	}
+
+	public void writeStateTo(Element node) {
+		throw new RuntimeException("not supported");
+	}
+
+	public File getPropertyFile() {
+		return null;
+	}
+
+	public File getConfigXMLFile() {
+		return cfgXmlFile;
+	}
+
+	public Properties getProperties() {
+		IEnvironment environment = getService().getEnvironment();
+		Properties p = new Properties();
+		p.setProperty(environment.getDialect(), "org.hibernate.dialect.HSQLDialect"); //$NON-NLS-1$
+		return p;
+	}
+
+	public File[] getMappingFiles() {
+		return new File[0];
+	}
+
+	public URL[] getCustomClassPathURLS() {
+		return new URL[0];
+	}
+
+	public String getName() {
+		return TestConsoleMessages.ConsoleConfigurationTest_fake_prefs;
+	}
+
+	public String getEntityResolverName() {
+		return ""; //$NON-NLS-1$
+	}
+
+	public ConfigurationMode getConfigurationMode() {
+		return ConfigurationMode.CORE;
+	}
+
+	public String getNamingStrategy() {
+		return null;
+	}
+
+	public String getPersistenceUnitName() {
+		return null;
+	}
+
+	public String getConnectionProfileName() {
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.hibernate.console.preferences.ConsoleConfigurationPreferences#getDialectName()
+	 */
+	public String getDialectName() {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.hibernate.console.preferences.ConsoleConfigurationPreferences#getHibernateVersion()
+	 */
+	@Override
+	public String getHibernateVersion() {
+		String[] versions = RuntimeServiceManager.getInstance().getAllVersions();
+		return versions[versions.length - 1];
+	}
+	
+	private IService getService() {
+		return RuntimeServiceManager.getInstance().findService(getHibernateVersion());
+	}
+
+}
