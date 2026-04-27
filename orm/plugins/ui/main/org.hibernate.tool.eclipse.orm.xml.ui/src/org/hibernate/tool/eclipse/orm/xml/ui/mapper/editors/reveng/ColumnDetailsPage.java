44ee28ecc41c60c0e163f2f5b5625670dff81e67
Koen Aers <koen.aers@gmail.com>
2026-04-27 16:16:47 +0200

    #5 Rename xml.core, xml.ui, and help.ui packages from org.hibernate.eclipse.* to new homes
    - xml.core: org.hibernate.eclipse.mapper.* -> org.hibernate.tool.eclipse.orm.xml.core.mapper.*
    - xml.ui: org.hibernate.eclipse.mapper.* -> org.hibernate.tool.eclipse.orm.xml.ui.mapper.*
    - help.ui: org.hibernate.eclipse.help -> org.hibernate.tool.eclipse.orm.help.ui
    
    Signed-off-by: Koen Aers <koen.aers@gmail.com>

---
+++ orm/plugins/ui/main/org.hibernate.tool.eclipse.orm.xml.ui/src/org/hibernate/tool/eclipse/orm/xml/ui/mapper/editors/reveng/ColumnDetailsPage.java
@@ -0,0 +1,124 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2005, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.hibernate.tool.eclipse.orm.xml.ui.mapper.editors.reveng;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.IDetailsPage;
+import org.eclipse.ui.forms.IFormPart;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.Section;
+import org.hibernate.tool.eclipse.orm.model.core.IRevEngColumn;
+import org.hibernate.tool.eclipse.orm.xml.core.nls.Messages;
+import org.hibernate.tool.eclipse.orm.xml.ui.mapper.editors.reveng.xpl.FormTextEntry;
+
+public class ColumnDetailsPage extends RevEngDetailsPage implements IDetailsPage, PropertyChangeListener {
+
+	//private Button excluded;
+	private FormTextEntry nameEntry;
+	private FormTextEntry jdbcTypeEntry;
+	private FormTextEntry propertyEntry;
+	private FormTextEntry typeEntry;
+	private IRevEngColumn column;
+
+	public void buildContents(FormToolkit toolkit, Section section, Composite client) {
+		section.setText(Messages.ColumnDetailsPage_column_details);
+		section.setDescription(Messages.ColumnDetailsPage_set_properties_of_selected_column);
+
+		nameEntry = new FormTextEntry(client, toolkit, Messages.ColumnDetailsPage_name, SWT.NULL);
+		nameEntry.setDescription(Messages.ColumnDetailsPage_the_name_of_the_column);
+		nameEntry.setFormEntryListener(new FormTextEntryListenerAdapter() {
+			public void textValueChanged(FormTextEntry entry) {
+				column.setName(entry.getValue());
+			}
+		});
+
+		//excluded = toolkit.createButton(client, Messages.ColumnDetailsPage_exclude_columns_from_reverse_engineering, SWT.CHECK);
+		GridData gridData = new GridData(SWT.FILL, SWT.FILL, true,false);
+		gridData.horizontalSpan = 3;
+		/*excluded.setLayoutData(gridData);
+		excluded.addSelectionListener(new SelectionAdapter() {
+
+			public void widgetSelected(SelectionEvent e) {
+				column.setExcluded(excluded.getSelection());
+			}
+
+		});*/
+
+		jdbcTypeEntry = new FormTextEntry(client, toolkit, Messages.ColumnDetailsPage_jdbc_type, SWT.NULL);
+		jdbcTypeEntry.setDescription(Messages.ColumnDetailsPage_which_jdbc_type_this_column_should_have);
+		jdbcTypeEntry.setFormEntryListener(new FormTextEntryListenerAdapter() {
+			public void textValueChanged(FormTextEntry entry) {
+				column.setJDBCType(entry.getValue());
+			}
+		});
+		propertyEntry = new FormTextEntry(client, toolkit, Messages.ColumnDetailsPage_property_name, SWT.NULL);
+		propertyEntry.setDescription(Messages.ColumnDetailsPage_the_property_name_which_must_be_used_for);
+		propertyEntry.setFormEntryListener(new FormTextEntryListenerAdapter() {
+			public void textValueChanged(FormTextEntry entry) {
+				column.setPropertyName(entry.getValue());
+			}
+		});
+
+		typeEntry = new FormTextEntry(client, toolkit, Messages.ColumnDetailsPage_hibernate_type, SWT.NULL);
+		typeEntry.setDescription(Messages.ColumnDetailsPage_the_hibernate_type);
+		typeEntry.setFormEntryListener(new FormTextEntryListenerAdapter() {
+			public void textValueChanged(FormTextEntry entry) {
+				column.setType(entry.getValue());
+			}
+		});
+	}
+
+	public void selectionChanged(IFormPart part, ISelection selection) {
+		IRevEngColumn newColumn = (IRevEngColumn) ((IStructuredSelection)selection).getFirstElement();
+		if(column!=null) {
+			column.removePropertyChangeListener(this);
+		}
+		if(newColumn!=null) {
+			newColumn.addPropertyChangeListener(this);
+		}
+		column = newColumn;
+
+		update();
+	}
+
+	private void update() {
+		nameEntry.setValue(column.getName());
+		jdbcTypeEntry.setValue(column.getJDBCType());
+		propertyEntry.setValue(column.getPropertyName());
+		typeEntry.setValue(column.getType());
+		//excluded.setSelection(column.getExclude());
+
+		nameEntry.setEditable(!column.getExclude());
+	}
+
+	public void propertyChange(PropertyChangeEvent evt) {
+		update();
+	}
+
+}
---
+++ orm/plugins/ui/main/org.hibernate.tool.eclipse.orm.xml.ui/src/org/hibernate/tool/eclipse/orm/xml/ui/mapper/extractor/ColumnNameHandler.java
@@ -0,0 +1,80 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2005, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.hibernate.tool.eclipse.orm.xml.ui.mapper.extractor;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jface.text.contentassist.CompletionProposal;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.hibernate.tool.eclipse.orm.console.core.eclipse.HibernateProjectConsoleManager;
+import org.hibernate.tool.eclipse.orm.runtime.spi.IColumn;
+import org.hibernate.tool.eclipse.orm.runtime.spi.ITable;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Node;
+
+public class ColumnNameHandler implements HBMInfoHandler {
+
+	private HBMInfoExtractor extractor;
+
+	ColumnNameHandler(HBMInfoExtractor hie) {
+		this.extractor = hie;
+	}
+	
+	public ICompletionProposal[] attributeCompletionProposals(
+			IJavaProject javaProject, Node node, String attributeName,
+			String start, int offset) {
+
+		List columns = new ArrayList();
+
+		String nearestTableName = extractor.getNearestTableName(node);
+		if(nearestTableName!=null) {
+			ITable table = HibernateProjectConsoleManager.getTable(javaProject, nearestTableName);
+			if (table!=null) {
+				Iterator tableMappings = table.getColumnIterator();
+				while (tableMappings.hasNext() ) {
+					IColumn column = (IColumn) tableMappings.next();
+					if(column.getName().toUpperCase().startsWith(start.toUpperCase()) ) {
+						columns.add(column);
+					}
+				}
+			}
+		}
+
+		List proposals = new ArrayList();
+		for (Iterator iter = columns.iterator(); iter.hasNext();) {
+			IColumn element = (IColumn) iter.next();
+			proposals.add(new CompletionProposal(element.getName(), offset, start.length(), element.getName().length(), null, null, null, null) );
+		}
+		
+		return (ICompletionProposal[]) proposals.toArray(new ICompletionProposal[proposals.size()]);
+	}
+
+	public IJavaElement getJavaElement(IJavaProject project, Node currentNode,
+			Attr currentAttrNode) {
+		return null;
+	}
+
+}
